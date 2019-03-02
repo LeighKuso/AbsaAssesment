d8b035ff6f03b2b6483636492df15715dbe5e84a
Ashleigh <leighkuso@gmail.com>
2019-03-02 19:04:10 +0200

    Completed Task2
    Created a new Project
    Scraped flawed Ideas
    Created a Test Case for each user added
    Verify table is displayed.

---
+++ AbsaAssesment/Task2/AbsaAssessmentTask2/AbsaAssessmentTask2/Methods.cs
@@ -0,0 +1,60 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbsaAssessmentTask2
+{
+    class Methods
+    {
+        public static void EnterFirstName(string FirstName)
+        {
+            WebDriver.Driver.FindElement(By.XPath("/html/body/div[3]/div[2]/form/table/tbody/tr[1]/td[2]/input")).SendKeys(FirstName);
+        }
+
+        public static void EnterLastName(string LastName)
+        {
+            WebDriver.Driver.FindElement(By.XPath("/html/body/div[3]/div[2]/form/table/tbody/tr[2]/td[2]/input")).SendKeys(LastName);
+        }
+
+        public static void EnterUserName(string UserName)
+        {
+            WebDriver.Driver.FindElement(By.XPath("/html/body/div[3]/div[2]/form/table/tbody/tr[3]/td[2]/input")).SendKeys(UserName);
+        }
+
+        public static void EnterPassword(string Password)
+        {
+            WebDriver.Driver.FindElement(By.XPath("/html/body/div[3]/div[2]/form/table/tbody/tr[4]/td[2]/input")).SendKeys(Password);
+        }
+
+        public static void EnterEmail(string email)
+        {
+            WebDriver.Driver.FindElement(By.XPath("/html/body/div[3]/div[2]/form/table/tbody/tr[7]/td[2]/input")).SendKeys(email);
+        }
+
+        public static void EnterPhoneNumber(string phone)
+        {
+            WebDriver.Driver.FindElement(By.XPath("/html/body/div[3]/div[2]/form/table/tbody/tr[8]/td[2]/input")).SendKeys(phone);
+        }
+
+        public static void SelectCompanyA()
+        {
+            WebDriver.Driver.FindElement(By.XPath("/html/body/div[3]/div[2]/form/table/tbody/tr[5]/td[2]/label[1]/input")).Click();
+        }
+
+        public static void SelectCompanyB()
+        {
+            WebDriver.Driver.FindElement(By.XPath("/html/body/div[3]/div[2]/form/table/tbody/tr[5]/td[2]/label[2]/input")).Click();
+        }
+
+        public static void SelectRole(IWebElement ele, string text)
+        {
+            SelectElement select = new SelectElement(ele);
+            select.SelectByText(text);
+        }
+    }
+}
---
+++ AbsaAssesment/Task2/AbsaAssessmentTask2/AbsaAssessmentTask2/Program.cs
@@ -0,0 +1,84 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+
+namespace AbsaAssessmentTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        [SetUp]
+        public void Initialize()
+        {
+            WebDriver.Driver = new ChromeDriver();
+            WebDriver.Driver.Navigate().GoToUrl("http://www.way2automation.com/angularjs-protractor/webtables/");
+            Console.WriteLine("URL Opened");
+            
+        }
+
+        [Test]
+        public void VerifyPage()
+        {
+            Assert.IsTrue(WebDriver.Driver.PageSource.Contains("First Name"));
+        }
+
+        [Test]
+        public void AddUser1()
+        {
+            WebDriver.Driver.FindElement(By.XPath("/html/body/table/thead/tr[2]/td/button")).Click();
+            Console.WriteLine("Open AddUser Form");
+
+            
+            Methods.EnterFirstName("FName1");
+            Methods.EnterLastName("LName1");
+            Methods.EnterUserName("User1");
+            Methods.EnterPassword("Pass1");
+            Methods.EnterEmail("Admin@mail.com");
+            Methods.EnterPhoneNumber("082555");
+            Methods.SelectCompanyA();
+            Methods.SelectRole(WebDriver.Driver.FindElement(By.XPath("/html/body/div[3]/div[2]/form/table/tbody/tr[6]/td[2]/select")), "Admin");
+            Console.WriteLine("Enter details to the form");
+
+            WebDriver.Driver.FindElement(By.XPath("/html/body/div[3]/div[3]/button[2]")).Click();
+            Console.WriteLine("Click Save");
+
+            Assert.Equals(WebDriver.Driver.FindElement(By.XPath("/html/body/table/tbody/tr[1]/td[1]")), "FName1");
+        }
+
+        [Test]
+        public void AddUser2()
+        {
+            WebDriver.Driver.FindElement(By.XPath("/html/body/table/thead/tr[2]/td/button")).Click();
+            Console.WriteLine("Open AddUser Form");
+
+            Methods.EnterFirstName("FName2");
+            Methods.EnterLastName("LName2");
+            Methods.EnterUserName("User2");
+            Methods.EnterPassword("Pass2");
+            Methods.EnterEmail("cusomter@mail.com");
+            Methods.EnterPhoneNumber("083444");
+            Methods.SelectCompanyB();
+            Methods.SelectRole(WebDriver.Driver.FindElement(By.XPath("/html/body/div[3]/div[2]/form/table/tbody/tr[6]/td[2]/select")), "Customer");
+            Console.WriteLine("Enter details to the form");
+
+            WebDriver.Driver.FindElement(By.XPath("/html/body/div[3]/div[3]/button[2]")).Click();
+            Console.WriteLine("Click Save");
+            
+            Assert.Equals(WebDriver.Driver.FindElement(By.XPath("/html/body/table/tbody/tr[1]/td[1]")), "FName2");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            WebDriver.Driver.Close();
+            Console.WriteLine("Browser Closed");
+        }
+
+        
+    }
+}
