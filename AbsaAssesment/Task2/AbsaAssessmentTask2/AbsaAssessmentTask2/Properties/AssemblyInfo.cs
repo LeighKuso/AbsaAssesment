020d7c347c28140ba714a08c8c40a6e8c30f2e78
Ashleigh <leighkuso@gmail.com>
2019-03-02 08:47:57 +0200

    Revert "Created "Browswers" class"
    This reverts commit cf2dac7bed5f3144327a9e939d9826b96d08b93e.

--- AbsaAssesment/Task2/AbsaAssessmentTask2/AbsaAssessmentTask2/Properties/AssemblyInfo.cs
+++ AbsaAssesment/Task2/AbsaAssessmentTask2/AbsaAssessmentTask2/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ada2bd63-509e-48ed-8e90-71a87c237ada")]
+[assembly: Guid("a779bc3a-210b-4cc2-b838-3989dea1fd97")]
 
 // Version information for an assembly consists of the following four values:
 //
